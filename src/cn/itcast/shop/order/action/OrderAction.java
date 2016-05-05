fd3413fddf949e13858d51f0249e8ea5be083034
dd701729nn <492210577@qq.com>
2016-05-05 17:07:43 +0800

    添加OrderAction

--- src/cn/itcast/shop/order/action/OrderAction.java
+++ src/cn/itcast/shop/order/action/OrderAction.java
@@ -1,5 +1,6 @@
 package cn.itcast.shop.order.action;
 
+import java.io.IOException;
 import java.util.Date;
 
 import org.apache.struts2.ServletActionContext;
@@ -11,6 +12,7 @@ import cn.itcast.shop.order.vo.Order;
 import cn.itcast.shop.order.vo.OrderItem;
 import cn.itcast.shop.user.vo.User;
 import cn.itcast.shop.utils.PageBean;
+import cn.itcast.shop.utils.PaymentUtil;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
@@ -43,6 +45,25 @@ public class OrderAction extends ActionSupport implements ModelDriven<Order> {
 
 	public void setPage(Integer page) {
 		this.page = page;
+	}
+
+	// 接收支付通道
+	private String pd_FrpId;
+
+	public void setPd_FrpId(String pd_FrpId) {
+		this.pd_FrpId = pd_FrpId;
+	}
+
+	// 接收付款成功后响应的数据
+	private String r6_Order;
+	private String r3_Amt;
+
+	public void setR6_Order(String r6_Order) {
+		this.r6_Order = r6_Order;
+	}
+
+	public void setR3_Amt(String r3_Amt) {
+		this.r3_Amt = r3_Amt;
 	}
 
 	// 生成订单的方法
@@ -84,6 +105,7 @@ public class OrderAction extends ActionSupport implements ModelDriven<Order> {
 		// 清空购物车
 		cart.clearCart();
 		// 2.将订单对象显示在页面上
+		// 通过值栈方式显示：因为Order显示的对象就是模型驱动的使用的对象
 		return "saveSuccess";
 	}
 
@@ -104,6 +126,68 @@ public class OrderAction extends ActionSupport implements ModelDriven<Order> {
 	public String findByOid() {
 		order = orderService.findByOid(order.getOid());
 		return "findByOidSuccess";
+	}
+
+	// 为订单付款的方法
+	public String payOrder() throws IOException {
+		// 修改订单
+		String p0_Cmd = "Buy"; // 业务类型:
+		String p1_MerId = "10001126856";// 商户编号:
+		String p2_Order = order.getOid().toString();// 订单编号:
+		String p3_Amt = "0.01"; // 付款金额:
+		String p4_Cur = "CNY"; // 交易币种:
+		String p5_Pid = ""; // 商品名称:
+		String p6_Pcat = ""; // 商品种类:
+		String p7_Pdesc = ""; // 商品描述:
+		String p8_Url = "http://localhost:8080/shop/order_callBack.action"; // 商户接收支付成功数据的地址:
+		String p9_SAF = ""; // 送货地址:
+		String pa_MP = ""; // 商户扩展信息:
+		String pd_FrpId = this.pd_FrpId;// 支付通道编码:
+		String pr_NeedResponse = "1"; // 应答机制:
+		String keyValue = "69cl522AV6q613Ii4W6u8K6XuW8vM1N6bFgyv769220IuYe9u37N4y7rI4Pl"; // 秘钥
+		String hmac = PaymentUtil.buildHmac(p0_Cmd, p1_MerId, p2_Order, p3_Amt,
+				p4_Cur, p5_Pid, p6_Pcat, p7_Pdesc, p8_Url, p9_SAF, pa_MP,
+				pd_FrpId, pr_NeedResponse, keyValue); // hmac
+		// 向易宝发送请求:
+		StringBuffer sb = new StringBuffer(
+				"https://www.yeepay.com/app-merchant-proxy/node?");
+		sb.append("p0_Cmd=").append(p0_Cmd).append("&");
+		sb.append("p1_MerId=").append(p1_MerId).append("&");
+		sb.append("p2_Order=").append(p2_Order).append("&");
+		sb.append("p3_Amt=").append(p3_Amt).append("&");
+		sb.append("p4_Cur=").append(p4_Cur).append("&");
+		sb.append("p5_Pid=").append(p5_Pid).append("&");
+		sb.append("p6_Pcat=").append(p6_Pcat).append("&");
+		sb.append("p7_Pdesc=").append(p7_Pdesc).append("&");
+		sb.append("p8_Url=").append(p8_Url).append("&");
+		sb.append("p9_SAF=").append(p9_SAF).append("&");
+		sb.append("pa_MP=").append(pa_MP).append("&");
+		sb.append("pd_FrpId=").append(pd_FrpId).append("&");
+		sb.append("pr_NeedResponse=").append(pr_NeedResponse).append("&");
+		sb.append("hmac=").append(hmac);
+
+		// 重定向:向易宝出发:
+		ServletActionContext.getResponse().sendRedirect(sb.toString());
+		return NONE;
+	}
+
+	// 付款成功后的转向页面
+	public String callBack() {
+		// 修改订单状态：已经付款
+		Order currOrder = orderService.findByOid(Integer.parseInt(r6_Order));
+		currOrder.setState(2);
+		orderService.update(currOrder);
+		// 在页面上显示付款成功的信息
+		this.addActionMessage("支付成功!订单编号为: " + r6_Order + " 付款金额为: " + r3_Amt);
+		return "msg";
+	}
+
+	// 修改订单的状态:
+	public String updateState() {
+		Order currOrder = orderService.findByOid(order.getOid());
+		currOrder.setState(4);
+		orderService.update(currOrder);
+		return "updateStateSuccess";
 	}
 
 }
